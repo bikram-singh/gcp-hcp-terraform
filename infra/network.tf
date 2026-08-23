4568d871a18ca652c350ebea89753788b6f5962e
bikram-singh <bikram23march@gmail.com>
2026-08-23 16:19:50 +0530

    terraform fmt: fix formatting

--- infra/network.tf
+++ infra/network.tf
@@ -2,9 +2,9 @@
 
 resource "google_compute_network" "vpc" {
   project                 = var.project_id
-  name                     = "gcphub-${var.environment}-vpc01"
-  auto_create_subnetworks  = false
-  routing_mode             = "REGIONAL"
+  name                    = "gcphub-${var.environment}-vpc01"
+  auto_create_subnetworks = false
+  routing_mode            = "REGIONAL"
 }
 
 resource "google_compute_subnetwork" "subnet" {
